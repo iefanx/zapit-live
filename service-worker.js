33d99e062242200dbf31ee3179cf1cf66c25e634
iefan <realiefan@gmail.com>
2023-04-20 04:00:16 +0530

    initial commit

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+      caches.open("zapit-cache").then((cache) => {
+        return cache.addAll([
+          "/",
+          "logo.png",
+          "paywall.css",
+          "app.js",
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener("fetch", (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
