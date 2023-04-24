d8e83835c8465a8798a3a627cb624b6964ff5cf9
Iefan <66840141+realiefan@users.noreply.github.com>
2023-04-25 02:20:28 +0530

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,39 +1,35 @@
-const CACHE_NAME = 'zapit-cache-v2';
-const FILES_TO_CACHE = [
-  '/',
-  'index.html',
-  'paywall.css',
-  'app.js',
-  'logo.png',
-  'icon.png',
-];
+const CACHE_NAME = 'my-website-v1';
 
-self.addEventListener('install', (event) => {
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
+    caches.keys().then(cacheNames => {
       return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
+        cacheNames.filter(cacheName => {
+          // Remove all cache entries that do not match the current cache name
+          return cacheName.startsWith('my-website-') && cacheName !== CACHE_NAME;
+        }).map(cacheName => {
+          return caches.delete(cacheName);
         })
       );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) {
+        // Return cached response
+        return response;
+      } else {
+        // Fetch new response and cache it
+        return fetch(event.request).then(response => {
+          return caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, response.clone());
+            return response;
+          });
+        });
+      }
     })
   );
 });
