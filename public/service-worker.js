2f69f5c75cb4b6d1927aa07a760bb1fc193aff49
iefan <realiefan@gmail.com>
2023-04-25 01:50:07 +0530

    cache

---
+++ public/service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'zapit-cache-v2';
+const FILES_TO_CACHE = [
+  '/',
+  'index.html',
+  'paywall.css',
+  'app.js',
+  'logo.png',
+  'icon.png',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
